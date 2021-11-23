26ac7bde9da4505dedc7a46e8c8cfb3a4bd381f9
Flor Marshall <flordmarshall@gmail.com>
2021-11-23 17:27:24 -0500

    wallet conditions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
@@ -8,6 +8,8 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+//state
+const [walletAddress, setWalletAddress] = useState(null);  
   /*
    * This function holds the logic for deciding if a Phantom Wallet is
    * connected or not
@@ -23,9 +25,13 @@ const App = () => {
           const response = await solana.connect({onlyIfTrusted: true})
 
           console.log('Connected with Publick key: ', response.publicKey.toString());
+
+
+          setWalletAddress(response.publicKey.toString)
         }
       } else {
         alert('Solana object not found! Get a Phantom Wallet 👻');
+
       }
     } catch (error) {
       console.error(error);
@@ -65,13 +71,13 @@ const App = () => {
 
   return (
     <div className="App">
-      <div className="container">
+      <div className={walletAddress ? 'authed-container' : 'container'}>
         <div className="header-container">
           <p className="header">🖼 GIF Portal</p>
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
-          {renderNotConnectedContainer()}
+          { !walletAddress && renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
@@ -83,7 +89,7 @@ const App = () => {
           >{`built on @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
-    </div>
+      </div>
   );
 };
 
