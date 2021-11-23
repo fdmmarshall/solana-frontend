4a94b28b5d9894a1ff4ccd0260bfc803ecd385a8
Flor Marshall <flordmarshall@gmail.com>
2021-11-23 16:51:52 -0500

    fix react error

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
-import React, {useEffect} from 'react-dom'
+
+import React, { useEffect } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
-// Constants
+// Change this up to be your Twitter if you want.
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  /*
+   * This function holds the logic for deciding if a Phantom Wallet is
+   * connected or not
+   */
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
 
-const checkIfWalletIsConnected = async () => {
-  try {
-    const {solana} = window;
-
-    if (solana) {
-      if(solana.isPhantom) {
-        console.log('Phantom wallet found');
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom wallet found!');
+        }
+      } else {
+        alert('Solana object not found! Get a Phantom Wallet 👻');
       }
-    } else {
-      alert('Solana object not found! Get a Phantom Wallet 👻');
+    } catch (error) {
+      console.error(error);
     }
-  } catch (error) {
-    console.log(error)
-  }
-};
-
-useEffect(() => {
-  const onLoad = async () => {
-    await checkIfWalletIsConnected();
   };
-  window.addEventListener('load', onLoad);
-  return () => window.removeEventListener('load', onLoad);
-}, []);
+
+  /*
+   * When our component first mounts, let's check to see if we have a connected
+   * Phantom Wallet
+   */
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
 
   return (
     <div className="App">
